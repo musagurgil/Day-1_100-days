a16d55387c16d0c6afa67fac13015fa3245f08cd
musagurgil <34582038+musagurgil@users.noreply.github.com>
2023-01-21 14:10:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('Musa')
+print ('21 Ocak 2023')
+print ("""
+Bugün 100 gunde Phthon egitimine basladim.
+Bu egitim sonra ne olacagini ve bana ne katacagini cok merak ediyorum.
+100 gun gorusmek uzere 
+""")
+print ('Cok iyi hissediyorum 🥳')
+print ('Egitim surecimi buradan takip edebilirisiniz replit.com/@musagurgil')
